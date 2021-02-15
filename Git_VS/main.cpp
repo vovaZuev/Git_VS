986d3fc2a8d9c91224f622abe144b32c36fb4592
vovaZuev <vovazuev@yandex.ru>
2021-02-15 23:52:16 +0700

    Add project files.

---
+++ Git_VS/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+void FillRand(int* arr, int n);
+void PrintArray(int* arr, int n);
+void PushBack(int* arr, int* n, int value);
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	cout << "Привет мир!" << endl;
+	
+	int n = 10;
+	int* arr = new int[n];
+	FillRand(arr, n);
+	PushBack(arr, &n, 999);
+	PrintArray(arr, n);
+
+	delete[] arr;
+}
+
+void FillRand(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void PrintArray(int* arr, int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+void PushBack(int* arr, int* n, int value)
+{
+	int* temp = new int[*n + 1];
+	for (int i = 0; i < *n; i++)
+	{
+		temp[i] = arr[i];
+	}
+	temp[*n] = value;
+	delete[] arr;
+	arr = temp;
+}
